79aa022a475d2d926603f27c01ef910349b69982
tan <tanmayrajkrishna@gmail.com>
2025-12-30 12:39:43 +0530

    realise that im a big fucking chud and fix the jump buffer and coyote time

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,38 +1,38 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const ACCELERATION = 850.0
-const AIR_ACCELERATION_MULT = 0.6
-const DEACCELERATION = 1000.0
-const JUMP_HEIGHT = - 500.0
+const max_speed = 300.0
+const acceleration = 1250.0
+const air_acceleration_multiplier = 0.6
+const deacceleration = 1000.0
+const max_jump_height = -500.0
 
 func _physics_process(delta: float) -> void:
-	var accel = ACCELERATION
+	var accel = acceleration
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		accel *= AIR_ACCELERATION_MULT
 
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = move_toward(velocity.x, SPEED * direction, accel * delta)
+		velocity.x = move_toward(velocity.x, max_speed * direction, accel * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, DEACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, 0, deacceleration * delta)
 	
 	if is_on_floor():
-		$CoyoteTimer.stop()
-	else:
 		$CoyoteTimer.start()
-		
-	if is_on_floor() or not $CoyoteTimer.is_stopped():
-		if Input.is_action_just_pressed("jump") or not $JumpBufferTimer.is_stopped():
-			velocity.y = JUMP_HEIGHT
-			$JumpBufferTimer.stop()
-	else:
-		if Input.is_action_just_pressed("jump"):
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or not $CoyoteTimer.is_stopped():
+			velocity.y = max_jump_height
+			$CoyoteTimer.stop()
+		else:
 			$JumpBufferTimer.start()
-		if Input.is_action_just_released("jump") and velocity.y < 0:
-			velocity.y *= 0.2
-		elif is_on_ceiling():
-			velocity.y *= 0.2
+	
+	if is_on_floor() and not $JumpBufferTimer.is_stopped():
+		velocity.y = max_jump_height
+		$JumpBufferTimer.stop()
+			
+	if (Input.is_action_just_released("jump") and velocity.y < 0) or is_on_ceiling():
+		velocity.y *= 0.2
 	
 	move_and_slide()
